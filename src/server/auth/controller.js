f445b9775707d752588c5c1ff43f5a5c21ebc61d
Sosthene NOUEBISSI <sosthenegolden@gmail.com>
2020-05-08 02:16:29 +0200

    update auth sign token controller

--- src/server/auth/controller.js
+++ src/server/auth/controller.js
@@ -1,9 +1,9 @@
-import { signToken } from "./auth";
+import auth from "./auth";
 
 export const signin = function (req, res, next) {
   // req.user will be there from the middleware
   // verify user. Then we can just create a token
   // and send it back for the client to consume
-  const token = signToken(req.user._id);
+  const token = auth.signToken(req.user._id);
   res.json({ token: token });
 };
